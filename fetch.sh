bfd3380e9af47cb0f676782cc7276de77437b66f
Supercip971 <supercyp971@gmail.com>
2022-07-06 21:22:44 +0000

    fetch: use exec rm instead of -delete

--- fetch.sh
+++ fetch.sh
@@ -37,6 +37,5 @@ cd libs/embed/
 # v = (path != ./*/*.h)  &&  (path == ./*.h)
 # delete(v) if (path != x86_64/**)  &&  (path != sdl/**)  &&  (path != posix/**)
 
-
-find . -type f \( \( -not -path './*/*.h' \) -and \( -path './*.h' \) -prune \) -o  -type f \( -not -path './x86_64/**' \) -and \( -not -path './sdl/**' \) -and \( -not -path './posix/**' \) -delete
+find . -type f \( \( -not -path './*/*.h' \) -and \( -path './*.h' \) -prune \) -o  -type f \( -not -path './x86_64/**' \) -and \( -not -path './sdl/**' \) -and \( -not -path './posix/**' \) -exec rm {} +
 cd ../..
